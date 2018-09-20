e52cd16ba916d167ed75fd5e1e65d89f2574f1c8
JDumadag25 <justin.dumadag@gmail.com>
2018-09-20 02:21:26 -0400

    starting to build homepage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import NavBar from './components/NavBar'
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,10 +7,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+        
+        <NavBar/>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
