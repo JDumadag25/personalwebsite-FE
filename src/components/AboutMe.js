aee3bd73d5ba16ff7f85e1b022d230364ab82a66
JDumadag25 <justin.dumadag@gmail.com>
2019-03-27 16:36:56 -0400

    gonna implement routes

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,14 +1,10 @@
 import React from 'react'
-import { Button } from 'semantic-ui-react'
+
 
 class AboutMe extends React.Component{
   render(){
     return(
-      <div>
-        <h1>JUSTIN DUMADAG</h1>
-        <h3>Software Developer</h3>
-        <Button>Resume</Button><Button>Contact</Button>
-      </div>
+    <h1>hello</h1>
     )
   }
 }
--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import AboutMe from './AboutMe'
+import { Button } from 'semantic-ui-react'
 
 class Homepage extends React.Component{
   render(){
     return(
-      <AboutMe/>
+      <div>
+        <h1>JUSTIN DUMADAG</h1>
+        <h3>Software Developer</h3>
+        <Button>About me</Button><Button>Contact</Button>
+      </div>
     )
   }
 }
