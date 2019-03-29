a790df6301a81a1ca8133afd5ba72d27f1315ce6
JDumadag25 <justin.dumadag@gmail.com>
2019-03-28 21:05:25 -0400

    Link to about me working

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
 import NavBar from './components/NavBar'
 import Homepage from './components/Homepage'
+import AboutMe from './components/AboutMe'
 import './App.css';
-import { BrowserRouter as Router} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Redirect, Switch, Link, withRouter} from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+       <Router>
+        <Switch>
+          <Route exact path="/" component={Homepage} />
 
-        <NavBar/>
-        <Homepage/>
+          <Route path="/aboutme" render={(props) => <AboutMe/>} />
+
+        </Switch>
+      </Router>
       </div>
     );
   }
--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,13 +1,12 @@
 import React from 'react'
 import AboutMe from './AboutMe'
 import { Button } from 'semantic-ui-react'
-import { BrowserRouter as Router, Route, Redirect, Switch, withRouter} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Redirect, Switch, Link, withRouter} from 'react-router-dom'
 
 class Homepage extends React.Component{
 
   handleAboutMe = () => {
-    console.log("hello");
-    return <Route path="/aboutme" render={(props) => <AboutMe/>} />
+  console.log("about me");
   }
 
   handleContact = () => {
@@ -19,7 +18,7 @@ class Homepage extends React.Component{
       <div>
         <h1>JUSTIN DUMADAG</h1>
         <h3>Software Developer</h3>
-        <Button onClick={this.handleAboutMe}>About me</Button><Button onClick={this.handleContact}>Contact</Button>
+         <Link to="/aboutme" className="btn btn-primary">About Me</Link><Button onClick={this.handleContact}>Contact</Button>
       </div>
     )
   }
