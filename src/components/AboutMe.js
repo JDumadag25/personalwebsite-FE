44402987b37539b705c5b0e59c552fcb176799cc
JDumadag25 <justin.dumadag@gmail.com>
2019-04-01 23:16:37 -0400

    drafting projects

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -23,9 +23,18 @@ class AboutMe extends React.Component{
        Full stack web developer with a goal to contribute to something greater in this world. Found my calling
        in web development where I can use my creativity and problem solving skills to build applications that can
        help empower people. I have knowledge in Ruby on Rails, JavaScript, React and Redux. Now I'm looking for an opportunity to
-       grown my skillset and work with passionate, dedicated people to create something amazing. 
+       grown my skillset and work with passionate, dedicated people to create something amazing.
      </p>
    </Container>
+   <br></br>
+   <br></br>
+   <br></br>
+   <Container>
+    <p>
+      What else besides Software development? <br></br>
+    Born and raised in New York with a short tour in Southern California. I enjoy 
+    </p>
+  </Container>
  </div>
     )
   }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Container } from 'semantic-ui-react'
 
 class Projects extends React.Component{
   render(){
     return(
-      <h1>Project placeholder from github</h1>
+      <div>
+        <Container>
+         <p>
+           PartyFy <br></br>
+         21st century Jukebox. Using the Spotify API, I created an application that allows users to search for songs and request for the
+         song to be added to the queue. Other party-goers can approve or veto a song. Built with a React frontend handling the calls to the API, and
+         a Rails backend in charge of Authentication and vote counting. Implemented ActionCable websocket for a chat feature to provide a more social feel. 
+         </p>
+       </Container>
+      </div>
     )
   }
 }
