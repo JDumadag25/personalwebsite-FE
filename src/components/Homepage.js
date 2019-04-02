e68e230b9c34624bba628048ffeb72fcecbe52e8
JDumadag25 <justin.dumadag@gmail.com>
2019-04-01 23:39:43 -0400

    added components to homepage for scrolling

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import AboutMe from './AboutMe'
+import Projects from './Projects'
+import Contact from './Contact'
 import { Button } from 'semantic-ui-react'
 import { BrowserRouter as Router, Route, Redirect, Switch, Link, withRouter} from 'react-router-dom'
 
@@ -11,6 +13,9 @@ class Homepage extends React.Component{
         <h1>JUSTIN DUMADAG</h1>
         <h3>Software Developer</h3>
         <Button as={Link} to='/aboutme'>ABOUT ME</Button><Button as={Link} to='/contact'>CONTACT</Button>
+        <AboutMe/>
+        <Projects/>
+        <Contact/>
       </div>
     )
   }
