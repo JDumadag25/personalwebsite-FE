799726cf9238177af3956f48687d3c7c5274f4d5
JDumadag25 <justin.dumadag@gmail.com>
2019-05-09 22:48:42 -0400

    slight detailing

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -361,29 +361,21 @@ const HomepageLayout = () => (
         <Grid divided inverted stackable>
           <Grid.Row>
             <Grid.Column width={3}>
-              <Header inverted as='h4' content='About' />
+              <Header inverted as='h4' content='LINKS' />
               <List link inverted>
-                <List.Item as='a'>Sitemap</List.Item>
-                <List.Item as='a'>Contact Us</List.Item>
-                <List.Item as='a'>Religious Ceremonies</List.Item>
-                <List.Item as='a'>Gazebo Plans</List.Item>
+                <List.Item as='a'>LinkedIn</List.Item>
+                <List.Item as='a'>GitHub</List.Item>
+                <List.Item as='a'>Medium</List.Item>
+                <List.Item as='a'>Email</List.Item>
               </List>
             </Grid.Column>
-            <Grid.Column width={3}>
-              <Header inverted as='h4' content='Services' />
-              <List link inverted>
-                <List.Item as='a'>Banana Pre-Order</List.Item>
-                <List.Item as='a'>DNA FAQ</List.Item>
-                <List.Item as='a'>How To Access</List.Item>
-                <List.Item as='a'>Favorite X-Men</List.Item>
-              </List>
-            </Grid.Column>
+
             <Grid.Column width={7}>
               <Header as='h4' inverted>
-                Footer Header
+                CONTACT ME
               </Header>
               <p>
-                Extra space for a call to action inside the footer that could help re-engage users.
+                Justin.Dumadag@gmail.com 
               </p>
             </Grid.Column>
           </Grid.Row>
